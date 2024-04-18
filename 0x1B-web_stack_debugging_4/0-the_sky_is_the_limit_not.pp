7e5c67b0c653a2a2fb704cbb34bdb0f32f6fc174
arsany199 <arsanykamal30@gmail.com>
2024-04-18 08:23:20 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,3 @@
-# this file increase the amount of traffic of a server can handle
-exec { 'limit to 2000':
-  command => "sed -i 's/15/2000' etc/default/nginx",
-  path    => '/bin'
-}
-
-exec { 'nginx restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
-}
+# Puppet script
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
