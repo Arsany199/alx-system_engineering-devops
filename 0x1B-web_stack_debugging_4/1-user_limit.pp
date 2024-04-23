4f6a382fac281885ef4c8c1097ef643cac14a5c4
arsany199 <arsanykamal30@gmail.com>
2024-04-23 06:00:15 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,2 @@
 # holberton user can login without any errors
-exec { 'increase hard limit':
-  command => "sed -i "s/5/2000" /etc/security/limits.conf",
-  path    => "/bin"
-}
-
-exec { 'increase soft limit':
-  command => "sed -i "s/4/2000" /etc/security/limits.conf",
-  path    => "/bin"
-}
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
